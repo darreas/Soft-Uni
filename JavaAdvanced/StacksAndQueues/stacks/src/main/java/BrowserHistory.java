dd5c1ff271ad185783f419133c949a83a818b696
darreas <s7efan.tsvetanov@gmail.com>
2023-03-05 19:12:56 +0200

    First Problem
    Java Advanced Stacks and Queues first problem solved

---
+++ JavaAdvanced/StacksAndQueues/stacks/src/main/java/BrowserHistory.java
@@ -0,0 +1,32 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BrowserHistory {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        String currentURL = "";
+
+        while (!input.equals("Home")){
+            if (input.equals("back")){
+                if (stack.size() > 1){
+                    stack.pop();
+                    currentURL = stack.peek();
+                    System.out.println(currentURL);
+                }else{
+                    System.out.println("no previous URLs");
+                }
+            }else {
+                stack.push(input);
+                currentURL = stack.peek();
+                System.out.println(currentURL);
+            }
+            input = scanner.nextLine();
+        }
+    }
+}
